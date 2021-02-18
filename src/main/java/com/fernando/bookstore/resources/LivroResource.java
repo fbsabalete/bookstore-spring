6128737204d53d7a345b7885e1a41a2fcf9f825b
Fernando Sabalete <fb.sabalete@gmail.com>
2021-02-18 14:14:46 -0300

    Ajustes finalizados no backend
    Updating pom.xml for Maven compiling
    
    Update pom.xml
    
    pom.xml update
    
    pom.xml update
    
    gitignore
    
    slf4j fixed
    
    Ajustes

--- src/main/java/com/fernando/bookstore/resources/LivroResource.java
+++ src/main/java/com/fernando/bookstore/resources/LivroResource.java
@@ -5,13 +5,14 @@ import com.fernando.bookstore.model.Livro;
 import com.fernando.bookstore.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -34,20 +35,20 @@ public class LivroResource {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+    public ResponseEntity<Livro> update(@PathVariable Integer id,@Valid @RequestBody Livro obj){
         Livro newObj = service.update(id, obj);
         return ResponseEntity.ok().body(newObj);
     }
 
     @PatchMapping(value = "/{id}")
-    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id,@Valid @RequestBody Livro obj){
         Livro newObj = service.update(id, obj);
         return ResponseEntity.ok().body(newObj);
     }
     
     @PostMapping
     public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
-                                        @RequestBody Livro obj){
+                                        @Valid @RequestBody Livro obj){
         Livro newObj = service.create(id_cat, obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
         return ResponseEntity.created(uri).build();
